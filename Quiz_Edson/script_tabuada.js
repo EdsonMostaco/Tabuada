6eff8a65fb485f554b32f2b3c2efcfeaeea46763
EdsonMostaco <83928217+EdsonMostaco@users.noreply.github.com>
2023-09-05 17:49:09 -0300

    Add files
    Atualização do projeto

--- Quiz_Edson/script_tabuada.js
+++ Quiz_Edson/script_tabuada.js
@@ -1,56 +1,68 @@
-let multiplicador = 0//Math.floor(Math.random()*10) +1
-let multiplicado = 0 //Math.floor(Math.random()*10) +1
+document.addEventListener('DOMContentLoaded', function() {
+    let eValor = document.getElementById('valor');
+    let eMultiplicador = document.getElementById('multiplicador');
+    const btnStart = document.getElementById('btnStart');
+    const btnStop = document.getElementById('btnStop');
 
-const quantity = document.getElementById('quantity-question')
-const quantityQuestion = quantity
+    btnStart.addEventListener('click', start);
+    btnStop.addEventListener('click', stop);
 
+    function start() {
+        const btnStart = document.getElementById('btnStart');
+        const questionContainer = document.getElementById('questionContainer');
+        const btnStop = document.getElementById('btnStop');
+        btnStart.classList.add('deactive');
+        questionContainer.classList.remove('deactive');
+        btnStop.classList.remove('deactive');
+        geraMultiplicacao();
+    }
 
+    function stop() {
+        const btnStart = document.getElementById('btnStart');
+        const questionContainer = document.getElementById('questionContainer');
+        const btnStop = document.getElementById('btnStop');
+        btnStart.classList.remove('deactive');
+        questionContainer.classList.add('deactive');
+        btnStop.classList.add('deactive');
+    }
 
+    function geraMultiplicacao() {
+        let valor = Math.floor(Math.random() * 10) + 1;
+        let multiplicador = Math.floor(Math.random() * 10) + 1;
+        console.log('valor: ', valor, ' multiplicado: ', multiplicador);
 
+        eValor.textContent = valor;
+        eMultiplicador.textContent = multiplicador;
 
+        let respostaCerta = valor * multiplicador;
 
+        const respostasIncorretas = [];
 
-
-
-
-
-
-
-
-
-//função que gera questões de multiplicação
-function geraMultiplicacao (multiplicador, multiplicado) {
-    multiplicador = Math.floor(Math.random()*10) +1
-    multiplicado = Math.floor(Math.random()*10) +1
-    respostaCorreta = multiplicador * multiplicado
-    respostas = []
-    respostas.push(respostaCorreta)
-    for (let i = 0 ; i < 4 ; i++) {
-        let respostaAleatoria = Math.floor(Math.random()*100) + 1
-        while(respostas.includes(respostaAleatoria)) {
-            respostaAleatoria = Math.floor(Math.random()*10) +1
+        while (respostasIncorretas.length < 4) {
+            const respostaIncorreta = Math.floor(Math.random() * 100) + 1;
+            if (
+                respostasIncorretas.indexOf(respostaIncorreta) === -1 &&
+                respostaIncorreta !== respostaCerta
+            ) {
+                respostasIncorretas.push(respostaIncorreta);
+                console.log(respostasIncorretas);
+            }
         }
-        respostas.push(respostaAleatoria)
-    }
-    respostas.sort(() => Math.random()-5)
+        console.log(respostasIncorretas);
 
-    let multiplicacao = {
-        multiplicador: multiplicador,
-        multiplicado: multiplicado,
-        respostaCorreta: respostaCorreta,
-        respostas: respostas
-    }
+        const respostas = [respostaCerta, ...respostasIncorretas];
 
-    return multiplicacao
-}
-
-function verificador (resposta) {
-    let respostaUsuario = resposta
-    console.log(respostaUsuario)
-    if( respostaUsuario == pergunta.respostaCorreta){
-        return true
-    }else{
-        return false
+        for (let i = respostas.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [respostas[i], respostas[j]] = [respostas[j], respostas[i]];
+        }
+        const botoes = document.querySelectorAll('.option-div');
+        for (let i = 0; i < botoes.length; i++) {
+            botoes[i].textContent = respostas[i];
+        }
     }
-        
-}
+    function checker() {
+        const botoes = document.querySelectorAll('.option-div');
+        console.log(botoes.value)
+    }
+});
